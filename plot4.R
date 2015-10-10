ac7b614d1d8f7398976fdbfa42c9ee4361bd7287
baletercero <pablotercero2011@yahoo.com>
2015-10-10 18:36:02 -0500

    First commit

--- plot4.R
+++ plot4.R
@@ -4,7 +4,11 @@ hpc$DateTime <- strptime(hpc$DateTime,'%d/%m/%Y %H:%M:%S')
 hpc$Date <- as.Date(hpc$Date,'%d/%m/%Y')
 hpc_sub <- hpc[hpc$Date>='2007-02-01'&hpc$Date<='2007-02-02',]
 # Create a histogram with the subst of data, color it red, name the x axis and the main title
-png(file = "plot3.png",width = 480, height = 480)
+png(file = "plot4.png",width = 480, height = 480)
+
+par(mfrow=c(2,2))
+
+with(hpc_sub,plot(DateTime,Global_active_power,type='l',xlab='',ylab='Global Active Power (kilowatts)'))
 
 with(hpc_sub,plot(DateTime,Voltage,xlab='datetime',type='l'))
 
@@ -14,5 +18,8 @@ with(hpc_sub,lines(DateTime,Sub_metering_2,col='red',pch=3,type='l'))
 with(hpc_sub,lines(DateTime,Sub_metering_3,col='blue',pch=3,type='l'))
 legend("topright",col=c("black", "red", "blue"),
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=0)
+
+with(hpc_sub,plot(DateTime,Global_reactive_power,xlab='datetime',type='l'))
+
 #dev.copy(png, file = "plot3.png",width = 480, height = 480)  ## Copy my plot to a PNG file
 dev.off()  ## Don't forget to close the PNG device!
